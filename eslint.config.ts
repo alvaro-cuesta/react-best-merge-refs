3ab0a207e6a7e7a0e4169630b7841b472f20ed63
Álvaro Cuesta <1827495+alvaro-cuesta@users.noreply.github.com>
2025-08-24 23:20:16 +0200

    fix: eslint if coverage is generated

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,49 +3,56 @@ import { globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(globalIgnores(['dist']), {
-  files: ['**/*.{ts,tsx}'],
-  extends: [
-    js.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-  ],
-  rules: {
-    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
-    '@typescript-eslint/dot-notation': [
-      'error',
-      {
-        allowIndexSignaturePropertyAccess: true,
-      },
-    ],
-    '@typescript-eslint/restrict-template-expressions': [
-      'error',
-      {
-        allow: [{ name: ['Error', 'URL', 'URLSearchParams'], from: 'lib' }],
-        allowAny: false,
-        allowArray: false,
-        allowBoolean: true,
-        allowNever: false,
-        allowNullish: true,
-        allowNumber: true,
-        allowRegExp: true,
-      },
-    ],
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
-    ],
+export default tseslint.config(
+  globalIgnores(['dist', 'coverage']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [js.configs.recommended],
   },
-  languageOptions: {
-    ecmaVersion: 2022,
-    globals: { ...globals.browser, ...globals.vitest },
-    parserOptions: {
-      project: [
-        './tsconfig.lib.json',
-        './tsconfig.test.json',
-        './tsconfig.node.json',
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+    ],
+    rules: {
+      '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+      '@typescript-eslint/dot-notation': [
+        'error',
+        {
+          allowIndexSignaturePropertyAccess: true,
+        },
       ],
-      tsconfigRootDir: import.meta.dirname,
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allow: [{ name: ['Error', 'URL', 'URLSearchParams'], from: 'lib' }],
+          allowAny: false,
+          allowArray: false,
+          allowBoolean: true,
+          allowNever: false,
+          allowNullish: true,
+          allowNumber: true,
+          allowRegExp: true,
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+      ],
+    },
+    languageOptions: {
+      ecmaVersion: 2022,
+      globals: { ...globals.browser, ...globals.vitest },
+      parserOptions: {
+        project: [
+          './tsconfig.lib.json',
+          './tsconfig.test.json',
+          './tsconfig.node.json',
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
-});
+);
